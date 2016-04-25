9d586f9d2f6cda9c8b8fb2c6e733a66e1d0ef05d
James Talmage <james@talmage.io>
2016-04-24 21:47:06 -0400

    refactor zen-observable specific tests a bit

--- test.js
+++ test.js
@@ -45,6 +45,7 @@ function *expectations(...args) {
 }
 
 function testWithImplementation(prefix, Observable) {
+	const isZen = Observable === ZenObservable;
 	const m = create(Observable);
 
 	test(`${prefix}: emits data events`, t => {
@@ -65,8 +66,8 @@ function testWithImplementation(prefix, Observable) {
 			.then(delay(10));
 	});
 
-	// RxJs does not honor this contract - resolves to null.
-	if (Observable === ZenObservable) {
+	// RxJs and Zen-Observable do not honor the same contract - RxJs resolves to null.
+	if (isZen) {
 		test(`${prefix}: forEach resolves with the value passed to the "end" event`, async t => {
 			t.plan(1);
 			var ee = new EventEmitter();
@@ -79,25 +80,26 @@ function testWithImplementation(prefix, Observable) {
 			var result = await m(ee).forEach(() => {});
 			t.is(result, 'fin');
 		});
-
-		test(`${prefix}: forEach resolves with the value of the awaited promise`, async t => {
-			t.plan(3);
-			var ee = new EventEmitter();
-			var awaited = deferred();
-			var expected = expectations('a', 'b');
-
-			emitSequence(ee, [
-				['data', 'a'],
-				['data', 'b'],
-				() => awaited.resolve('resolution'),
-				['data', 'c']
-			]);
-
-			var result = await m(ee, {endEvent: false, await: awaited})
-				.forEach(chunk => t.is(chunk, expected.next().value));
-			t.is(result, 'resolution');
-		});
 	}
+
+	test(`${prefix}: forEach resolves after resolution of the awaited promise${isZen ? ', with promise value' : ''}`, async t => {
+		t.plan(3);
+		var ee = new EventEmitter();
+		var awaited = deferred();
+		var expected = expectations('a', 'b');
+
+		emitSequence(ee, [
+			['data', 'a'],
+			['data', 'b'],
+			() => awaited.resolve('resolution'),
+			['data', 'c']
+		]);
+
+		var result =
+			await m(ee, {endEvent: false, await: awaited})
+				.forEach(chunk => t.is(chunk, expected.next().value));
+		t.is(result, isZen ? 'resolution' : undefined);
+	});
 
 	test(`${prefix}: rejects on error events`, t => {
 		t.plan(3);
