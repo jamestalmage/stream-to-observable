e544a690538ef8f2ff8bd365a6842e7d9d49b3fa
James Talmage <james.talmage@jrtechnical.com>
2016-06-29 17:48:34 -0400

    use any-observable (#6)
    * use any-observable
    
    * PR feedback

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+var Observable = require('any-observable');
 
 function or(option, alternate, required) {
 	var result = option === false ? false : option || alternate;
@@ -10,71 +11,69 @@ function or(option, alternate, required) {
 	return result;
 }
 
-module.exports = function create(Observable) {
-	return function (stream, opts) {
-		opts = opts || {};
+module.exports = function (stream, opts) {
+	opts = opts || {};
 
-		var complete = false;
-		var dataListeners = [];
-		var awaited = opts.await;
-		var dataEvent = or(opts.dataEvent, 'data', true);
-		var errorEvent = or(opts.errorEvent, 'error');
-		var endEvent = or(opts.endEvent, 'end');
+	var complete = false;
+	var dataListeners = [];
+	var awaited = opts.await;
+	var dataEvent = or(opts.dataEvent, 'data', true);
+	var errorEvent = or(opts.errorEvent, 'error');
+	var endEvent = or(opts.endEvent, 'end');
 
-		function cleanup() {
-			complete = true;
-			dataListeners.forEach(function (listener) {
-				stream.removeListener(dataEvent, listener);
-			});
-			dataListeners = null;
+	function cleanup() {
+		complete = true;
+		dataListeners.forEach(function (listener) {
+			stream.removeListener(dataEvent, listener);
+		});
+		dataListeners = null;
+	}
+
+	var completion = new Promise(function (resolve, reject) {
+		function onEnd(result) {
+			if (awaited) {
+				awaited.then(resolve);
+			} else {
+				resolve(result);
+			}
 		}
 
-		var completion = new Promise(function (resolve, reject) {
-			function onEnd(result) {
-				if (awaited) {
-					awaited.then(resolve);
-				} else {
-					resolve(result);
-				}
-			}
+		if (endEvent) {
+			stream.once(endEvent, onEnd);
+		} else if (awaited) {
+			onEnd();
+		}
 
-			if (endEvent) {
-				stream.once(endEvent, onEnd);
-			} else if (awaited) {
-				onEnd();
-			}
+		if (errorEvent) {
+			stream.once(errorEvent, reject);
+		}
 
-			if (errorEvent) {
-				stream.once(errorEvent, reject);
-			}
+		if (awaited) {
+			awaited.catch(reject);
+		}
+	}).catch(function (err) {
+		cleanup();
+		throw err;
+	}).then(function (result) {
+		cleanup();
+		return result;
+	});
 
-			if (awaited) {
-				awaited.catch(reject);
-			}
-		}).catch(function (err) {
-			cleanup();
-			throw err;
-		}).then(function (result) {
-			cleanup();
-			return result;
-		});
+	return new Observable(function (observer) {
+		if (!complete) {
+			var onData = function onData(data) {
+				observer.next(data);
+			};
+			stream.on(dataEvent, onData);
+			dataListeners.push(onData);
+		}
 
-		return new Observable(function (observer) {
-			if (!complete) {
-				var onData = function onData(data) {
-					observer.next(data);
-				};
-				stream.on(dataEvent, onData);
-				dataListeners.push(onData);
-			}
-
-			completion
-				.catch(function (err) {
-					observer.error(err);
-				})
-				.then(function (result) {
-					observer.complete(result);
-				});
-		});
-	};
+		completion
+			.catch(function (err) {
+				observer.error(err);
+			})
+			.then(function (result) {
+				observer.complete(result);
+			});
+	});
 };
